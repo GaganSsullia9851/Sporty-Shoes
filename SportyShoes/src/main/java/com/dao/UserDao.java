8a75ddc3aa08904a8720fc4638bacf5cfda4f556
Gagan S <gagan.s@COMVIVA.COM>
2021-11-08 12:40:45 +0530

    SportyShoe Phase-3

---
+++ SportyShoes/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.bean.Users;
+
+@Repository
+public interface UserDao extends JpaRepository<Users, Integer>{	
+	@Query("select count(e)>0 from Users e where e.email=?1")
+	public boolean  userExits(String email);
+	
+}
